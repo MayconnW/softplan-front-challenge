db0cfbdfc28cfdef3d85aae832a8ee57a595c65a
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-08-12 23:08:45 -0300

    Removing unnecessary files and code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
+import dotenv from 'dotenv';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+dotenv.config();
+
+const App: React.FC = () => {
+  return <h1>Ola</h1>;
+};
 
 export default App;
